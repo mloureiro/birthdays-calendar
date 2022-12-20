3ec81860b17ab6d661ac561982b696c717f14903
Marcos Loureiro <marcos.loureiro@cosuno.de>
2022-12-20 23:55:19 +0000

    Define the endpoint to clear the db

--- routes/api.py
+++ routes/api.py
@@ -1,7 +1,16 @@
-from flask import Blueprint
+from flask import Blueprint, make_response
+from db import reset_db as db_reset_db
 
 api = Blueprint('api', __name__)
 
 @api.route('/')
 def main():
   return 'api'
+
+@api.route('/reset-db/<pwd>', methods=['POST'])
+def reset_db(pwd):
+  try:
+    db_reset_db(pwd)
+    return make_response('Done', 204)
+  except:
+    return make_response('Not found', 404)
