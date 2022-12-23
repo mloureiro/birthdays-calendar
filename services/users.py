1001a92fc3644a470d2dcc08a7c0bf539f4e8d50
Marcos Loureiro <marcos.loureiro@cosuno.de>
2022-12-23 23:17:55 +0000

    Handle user validation with unique errors

--- services/users.py
+++ services/users.py
@@ -45,4 +45,25 @@ def get_by_email(email):
 
     return user_list[0]
   except:
-    return None
+    return False
+
+
+def validate_user_details(first_name, last_name, email, password, birthdate):
+  if not is_valid_name(first_name):
+    raise UserDetailsInvalidException("Name", first_name)
+  if not is_valid_name(last_name):
+    raise UserDetailsInvalidException("Name", last_name)
+  if not is_valid_email(email):
+    raise UserDetailsInvalidException("Email", email)
+  if not is_valid_password(password, strict=False):
+    raise UserDetailsInvalidException("Password")
+  if not is_valid_birthdate(birthdate):
+    raise UserDetailsInvalidException("Birthdate", birthdate)
+
+
+class UserDetailsInvalidException(Exception):
+  def __init__(self, key, value=None, *args):
+    message = f"{key} is invalid" if value is None else f"{key} as '{value}' is invalid"
+    print('[message]>>', message)
+    super().__init__(message, args)
+
