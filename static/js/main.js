bb3a7c2d4798b6741d31c7e4e7b11f435200d4a1
Marcos Loureiro <marcos.loureiro@cosuno.de>
2022-12-24 08:44:44 +0000

    Spacing fix

--- static/js/main.js
+++ static/js/main.js
@@ -21,12 +21,12 @@
 
     const availableMessages = [
       // [key, background]
-      ['failure', 'linear-gradient(to right, #21b04b, #09A837)'],
-      ['success', 'linear-gradient(to right, #195ac0, #0048ba)'],
-      ['message', 'linear-gradient(to right, #d63232, #cc0000)'],
+      ['success', 'linear-gradient(to right, #21b04b, #09A837)'],
+      ['message', 'linear-gradient(to right, #195ac0, #0048ba)'],
+      ['failure', 'linear-gradient(to right, #d63232, #cc0000)'],
     ].map(([key, color]) => [getQueryParams().get(key), color]);
 
-    const [message, background] = availableTypes.find(([message]) => !!message) || [];
+    const [message, background] = availableMessages.find(([message]) => !!message) || [];
 
     if (!message) return;
 
@@ -37,6 +37,7 @@
       position: "center",
       stopOnFocus: true,
       style: { background },
+      onClick: () => null,
     }).showToast();
   })();
 
